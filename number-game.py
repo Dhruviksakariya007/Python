7470e2fd2b4a091245a249e20c01566089ea725c
Dhruvik Sakariya <dhruviksakariya007@gmail.com>
2024-12-22 22:28:06 +0530

    Updated number game

--- number-game.py
+++ number-game.py
@@ -13,20 +13,22 @@ rnd_number = randint(1, 20)
 
 Guesses_no = 0
 
+print("You have only 3 tries...!")
+print()
+
 try:
     while Guesses_no < 3:
         guess = input("Gussed number is :- ")
+        Guesses_no = Guesses_no + 1
 
         if int(guess) > rnd_number:
             print("number is too high")
             print("Try again..!")
-            # Guesses_no = Guesses_no + 1
             print()
 
         elif int(guess) < rnd_number:
             print("Number is too low")
             print("Try again..!")
-            
             print()
 
         else:
@@ -35,11 +37,9 @@ try:
             print("That number is " + str(rnd_number))
             break
 
-        Guesses_no = Guesses_no + 1
-
         if Guesses_no == 3:
             raise Exception()
 
 except:
-    print("That number was " + str(rnd_number))
+    print("The number was " + str(rnd_number))
     print("You've lost..!")
