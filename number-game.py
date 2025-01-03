5632da801ef07d199f68e22f88b5981a968a6397
Dhruvik Sakariya <dhruviksakariya007@gmail.com>
2025-01-03 18:49:59 +0530

    validtions

--- number-game.py
+++ number-game.py
@@ -19,26 +19,32 @@ print()
 try:
     while Guesses_no < 3:
         guess = input("Gussed number is :- ")
-        Guesses_no = Guesses_no + 1
 
-        if int(guess) > rnd_number:
-            print("number is too high")
-            print("Try again..!")
-            print()
+        if guess.isdigit():
 
-        elif int(guess) < rnd_number:
-            print("Number is too low")
-            print("Try again..!")
-            print()
+            Guesses_no = Guesses_no + 1
 
+            if int(guess) > rnd_number:
+                print("number is too high")
+                print("Try again..!")
+                print()
+
+            elif int(guess) < rnd_number:
+                print("Number is too low")
+                print("Try again..!")
+                print()
+
+            else:
+                print()
+                print("Good job! you've guessed in " + str(Guesses_no) + " try..!")
+                print("That number is " + str(rnd_number))
+                break
+
+            if Guesses_no == 3:
+                raise Exception()
+            
         else:
-            print()
-            print("Good job! you've guessed in " + str(Guesses_no) + " try..!")
-            print("That number is " + str(rnd_number))
-            break
-
-        if Guesses_no == 3:
-            raise Exception()
+            print("Please enter digits..!\n")
 
 except:
     print("The number was " + str(rnd_number))
