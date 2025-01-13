69284dc83c03db3e4566ad486d3e99507ec0ce23
Dhruvik Sakariya <dhruviksakariya007@gmail.com>
2025-01-13 20:14:25 +0530

    String's functions

---
+++ strings in python.py
@@ -0,0 +1,15 @@
+spam = "hello world"
+
+print(f'is capitalized ? {spam.capitalize()}')
+
+print(f'is upper ? {spam.upper().isupper()}')
+
+print(f'is decimal ? {spam.isdecimal()}')
+
+print(f'White space ? {spam[5].isspace()}')
+
+print(f'is title ? {spam.istitle()}')
+ 
+print(f'is alpha ? {spam.isalpha()}')
+
+print(f'is alnum () alpha & number ? {spam.isalnum()}')
