f3fa68e11d130cdc7c97a4b95e7cafc13d235224
Dhruvik Sakariya <dhruviksakariya007@gmail.com>
2025-01-16 21:37:04 +0530

    update regx

--- regexinpy.py
+++ regexinpy.py
@@ -2,11 +2,12 @@ import re
 
 msg = "calling in +91 8128574360 means +91 8128574361"
 
-phoneRegx = re.findall(r"[+]\d\d \d\d\d\d\d\d\d\d\d\d", msg) # \d is for digits
+phoneRegx = re.search(r"([+]\d\d) (\d\d\d\d\d\d\d\d\d\d)", msg) # \d is for digits
 
 try:
-    print(f'{phoneRegx}')
+    print(f'{phoneRegx.group()}')
 
 except:
     print(f'Phone number is not found..!')
     
+print(f'{phoneRegx.group(1)}')
